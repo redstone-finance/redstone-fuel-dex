741b6235a1dcc1b207c7395a23d573cfed0eefc9
Łukasz Kalbarczyk <l.kalbarczyk@gmail.com>
2023-06-12 12:38:13 +0200

    fix contract id

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-const IS_LOCAL = true;
+const IS_LOCAL = false;
 
 export const FUEL_RPC_URL = IS_LOCAL
   ? "http://127.0.0.1:4000/graphql"
@@ -15,6 +15,6 @@ export const FUEL_TX_EXPLORER_URL =
 export const FUEL_TOKEN_ID =
   "0x6cb020a8d81d9394b9b3c70e0994b33835d43dd8069b0e427be574a2ee3c3437";
 export const FUEL_DEX_CONTRACT_ID =
-  "0x55797523ba8c98e0187a4b6db622f2c62bc2ad90c04a055c3910ee65842da792";
+  "0x6d98583228051ce2a14968daad63b7bf22f3edb0af02e75b68da017d037b1412";
 export const FUEL_DEX_CONTRACT_OWNER =
   "fuel16hn6vhqxxnn0f7ft8zskmu8759rndwzyl67eqqnj2neqpz7ln8yqm40tkm";
